569dc0ba0999cdc13a8a3c666874048632c1a8f9
Azeroqyu <zerosucksatlife@gmail.com>
2025-05-15 19:57:16 -0300

    Remodeling the logic of a few classes

--- src/Student.java
+++ src/Student.java
@@ -4,13 +4,13 @@ import java.util.List;
 public class Student {
 	private String name;
 	private String id;
-	private ArrayList<Course> courses;
+	protected List<Class> classes;
 	boolean withdrawn;
 
 	public Student(String name, String id) {
 		this.name = name;
 		this.id = id;
-		this.courses = new ArrayList<>();
+		this.classes = new ArrayList<>();
 	}
 
 	public void setId(String id) {
@@ -25,16 +25,31 @@ public class Student {
 		return id;
 	}
 
-	public ArrayList<Course> getCourses() {
-		return courses;
-	}
-
-	void signClass() {
+	public void CheckRegistedclasses(ArrayList<Class> courses) {
 
 	}
 
-	void withdrawClass() {
+	public void lisClasses(List<Class> classes, Class class1) {
+		classes.add(class1);
+	}
 
+	public void signClass(Class class1) {
+		classes.add(class1);
+	}
+
+	public void withdrawClass(List<Class> classes, Class class1) {
+		classes.remove(class1);
+	}
+
+	public void withdrawSemester(String semester, List<Class> classes) {
+		for (Class class1 : classes) {
+			if (class1.getSemester().equals(semester)) {
+				class1.setWithdrawn(true);
+			}
+		}
+	}
+
+	void withdrawSemester() {
 	}
 
 }
