1f23e983047573a1b5c9f59956303d9f900b38de
Azeroqyu <zerosucksatlife@gmail.com>
2025-05-17 20:20:17 -0300

    Removed useless classes

--- src/TeacherTools.java
+++
@@ -1,7 +0,0 @@
-public abstract class TeacherTools {
-	private Teacher ownerTeacher;
-
-	public Teacher getOwnerTeacher() {
-		return ownerTeacher;
-	}
-}
--- src/University.java
+++
@@ -1,6 +0,0 @@
-public class University {
-	University() {
-
-	}
-
-}
