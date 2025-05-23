8295032d8ac52046f818b601441e46f846cef6d2
Azeroqyu <zerosucksatlife@gmail.com>
2025-05-23 12:21:26 -0300

    creating the skeleton for the pannel system

---
+++ src/GenericPannel.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+import java.awt.*;
+import java.io.*;
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class GenericPannel {
+}
